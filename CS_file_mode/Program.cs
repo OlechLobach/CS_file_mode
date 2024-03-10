cb62f64253c32f93efd439c0bb2c40bb17d0296d
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-11 01:40:30 +0200

    Add project files.

---
+++ CS_file_mode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_file_mode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
